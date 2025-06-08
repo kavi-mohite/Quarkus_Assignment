551e0da6283066763a29929fb193d0fe15dd1613
kavi-mohite <mohitekavita16@gmail.com>
2025-06-08 09:39:35 +0530

    Add files via upload

--- src/main/respository/ProductRespository.java
+++ src/main/respository/ProductRespository.java
@@ -1,12 +1,21 @@
 package respository;
 
+import java.util.List;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 //import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import model.Product;
 
 @ApplicationScoped
-public class ProductRespository implements PanacheRepositoryBase<Product, Long> {
+public class ProductRespository /* extends PanacheEntity */  implements PanacheRepositoryBase<Product, Long>   {
+
+	public Uni<List<Product>> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
 
