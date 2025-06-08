6f44b83b26373355f6f09bdc90b0cf5807859413
kavi-mohite <mohitekavita16@gmail.com>
2025-06-08 09:37:03 +0530

    Add files via upload

--- src/main/model/Product.java
+++ src/main/model/Product.java
@@ -1,23 +1,35 @@
 package model;
 
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 //import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 //import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 //import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Column;
+import io.smallrye.mutiny.Uni;
 @Entity
-
-public class Product extends PanacheEntityBase {
-	 	@Id
-	 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	    public Long id;
+@Table(name="Product")
+public class Product extends PanacheEntity {
+	 	//@Id
+//	 	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="product_seq")
+//	 	@SequenceGenerator(name="product_seq", sequenceName = "product_seq", allocationSize = 1)
+//	    private Long id;
+	 	@Column(nullable = false)
 	    public String name;
+	 	@Column(nullable = false)
 	    public String description;
+	 	@Column(nullable = false)
 	    public Double price;
+	 	@Column(nullable = false)
 	    public int quantity;
 	
+	 	public static Uni<Product> findByName(String name){
+	 		return find("name",name).firstResult();
+	 	}
 }
